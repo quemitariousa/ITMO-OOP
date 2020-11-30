3759ac566e8019a6d63f5cc91751db4c5fccc760
Que <quemitariousa6@gmail.com>
2020-11-30 03:20:09 +0300

    add missing file

---
+++ Backup-OOP/ArchiveFileInformation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Backup_OOP
+{
+    public class ArchiveFileInformation : FileInformation
+    {
+        private List<FileInformation> _files;
+        public ArchiveFileInformation(string path, List<FileInformation> files) : base(files.Sum(x => x.Size), path)
+        {
+            _files = files;
+        }
+    }
+}
